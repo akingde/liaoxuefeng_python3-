8c1919e541d2ee0213835e5c32355308cb98ef68
李金熹 <32548701+TXLJX@users.noreply.github.com>
2019-12-18 00:12:25 +0800

    优化代码质量
    放上了所有教程，爬取速度加快

--- Crawl.py
+++ Crawl.py
@@ -1,24 +1,212 @@
-#-*-coding:utf-8-*-
-#@author:lijinxi
-'''
-获取所有urls
-'''
-import  requests
-from  bs4 import  BeautifulSoup
+# -*-coding:utf-8-*-
+# @author:lijinxi
 
-url=r'https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000'
-header={
-    'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
-}
-response=requests.get(url,headers=header)
-bsObj=BeautifulSoup(response.text,'html5lib')
-menu=bsObj.find('ul',{'id':'x-wiki-index','class':'uk-nav uk-nav-side'})
-num=0
-with open('url.txt','w') as f:
-    for link in menu.findAll('a',{'class':'x-wiki-index-item'}):
-        num+=1;
-        if link.attrs['href'] is not None:
-            absolutelink='https://www.liaoxuefeng.com'+link.attrs['href']
-            #absolutelink.encode('utf-8')
-            f.write(absolutelink+'\n')
-print('%d urls '%num);
+import requests
+from bs4 import BeautifulSoup
+import time
+import os
+import pdfkit
+import xlwt
+
+
+class Crawl:
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome'
+                      '/74.0.3729.28 Safari/537.36 OPR/61.0.3298.6 (Edition developer)',
+    }
+
+    templates = """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        {css}
+        {js}
+    </head>
+    <body>
+    {content}
+    </body>
+    </html>"""
+
+    start_time = time.time()
+
+    def __init__(self, start_url, pdf_to_save_name, css_save_path='./static/css/',
+                 js_save_path='./static/js/',
+                 pictures_save_path='./files/attachments/'):
+        self.start_url = start_url
+        self.pdf_to_save_name = pdf_to_save_name
+        self.css_save_path = css_save_path
+        self.picture_save_path = pictures_save_path
+        self.contents = []
+        self.css = []
+        self.titles = []
+        self.js = []
+
+        if not os.path.exists(self.css_save_path):
+            os.makedirs(self.css_save_path)
+
+        self.start()
+
+    def start(self):
+        response = requests.get(self.start_url, headers=self.headers)
+        bs_obj = BeautifulSoup(response.text, 'html5lib')
+        bs_obj.declared_html_encoding = 'utf-8'
+        menu = bs_obj.find('ul', {'id': 'x-wiki-index', 'class': 'uk-nav uk-nav-side'})
+        head = bs_obj.find('head')
+        styles = bs_obj.findAll('style')
+        for style in styles:
+            if style not in self.css:
+                self.css.append(style)
+        # save  external css
+        stylesheet = head.findAll('link', {'rel': 'stylesheet'})
+        if stylesheet is not None:
+            for sheet in stylesheet:
+                if sheet['href'] not in self.css:
+                    res = requests.get('https://static.liaoxuefeng.com' + sheet['href'], sheet['href'])
+                    if sheet['href'] == '/static/css/main.css?v=1.0-b1b83dc-2019-05-25T01:51:58Z':
+                        sheet['href'] = '/static/css/main.css'
+                    with open('.' + sheet['href'], 'w', encoding='utf-8') as f:
+                        f.write(res.text)
+                    sheet['href'] = '.' + sheet['href']
+                    self.css.append(sheet)
+
+        # external js
+        javascripts = head.findAll('script', attrs={'src': True, 'async': False, 'charset': False})
+        if javascripts is not None:
+            for javascript in javascripts:
+                if javascript['src'] not in self.css:
+                    res = requests.get('https://static.liaoxuefeng.com' + javascript['src'], javascript['src'])
+                    if not os.path.exists(os.path.dirname('.' + javascript['src'])):
+                        os.makedirs(os.path.dirname('.' + javascript['src']))
+                    if '?' in javascript['src']:
+                        continue
+                    with open('.' + javascript['src'], 'w', encoding='utf-8') as f:
+                        f.write(res.text)
+                    javascript['src'] = '.' + javascript['src']
+                    self.js.append(javascript)
+
+        for link in menu.findAll('a', {'class': 'x-wiki-index-item'}):
+            if link.attrs['href'] is not None:
+                absolute_link = 'https://www.liaoxuefeng.com' + link.attrs['href']
+                self.get_content(absolute_link)
+        self.xls()
+        self.template_format()
+        self.save_to_pdf()
+
+    def get_content(self, url):
+        print("正在抓取：{url}，当前耗时：{time}。".format(url=url, time=time.time() - self.start_time))
+        try:
+            response = requests.get(url, headers=self.headers)
+            assert response.status_code == 200
+        except AssertionError as e:
+            print(e)
+        except TimeoutError as e:
+            print(e)
+        except Exception as e:
+            print(e)
+        else:
+            bs_obj = BeautifulSoup(response.text, 'html5lib')
+            bs_obj.declared_html_encoding = 'utf-8'
+            # handle content
+            content = bs_obj.find('div', {'class': 'x-wiki-content x-main-content'})
+
+            # delete video
+            videos = bs_obj.findAll('iframe')
+            if videos is not None:
+                for video in videos:
+                    video.extract()
+
+            # save pictures
+            pictures = content.find_all('img')
+            for picture in pictures:
+                real_url = picture.attrs['data-src']
+                del picture.attrs['data-src']
+                img = requests.get('https://static.liaoxuefeng.com' + real_url)
+                dirname = os.path.dirname(real_url)
+                if not os.path.exists('.' + dirname):
+                    os.makedirs('.' + dirname)
+                with open('.' + real_url + '.jpg', 'wb') as f:
+                    f.write(img.content)
+                picture.attrs['src'] = '.' + real_url + '.jpg'
+
+            # save title
+            title = bs_obj.find('h4').get_text()
+            title_tag = bs_obj.new_tag('h1')
+            title_tag.attrs['style'] = 'text-align:center'
+            title_tag.string = title
+            self.titles.append(title)
+            self.contents.append(title_tag)
+            self.contents.append(content)
+
+            time.sleep(1)
+            print("抓取完成，文章名为：{title}。".format(title=title))
+
+    def save_to_pdf(self):
+        options = {
+            'page-size': 'Letter',
+            'margin-top': '0.75in',
+            'margin-right': '0.75in',
+            'margin-bottom': '0.75in',
+            'margin-left': '0.75in',
+            'encoding': "UTF-8",
+            'custom-header': [
+                ('Accept-Encoding', 'gzip')
+            ],
+            'cookie': [
+                ('cookie-name1', 'cookie-value1'),
+                ('cookie-name2', 'cookie-value2'),
+            ],
+            'outline-depth': 10,
+        }
+
+        toc = {
+            'xsl-style-sheet': 'toc.xls'
+        }
+        '''
+        cover = 'cover.html'
+        '''
+        pdfkit.from_file('templates.html', self.pdf_to_save_name, options=options)
+
+        # pdfkit.from_file('file.html', options=options, toc=toc, cover=cover, cover_first=True)
+
+    def xls(self):
+        wb = xlwt.Workbook()
+        ws = wb.add_sheet('目录')
+        num = 0
+        for title in self.titles:
+            ws.write(num, 0, title.strip())
+            num += 1
+        wb.save('toc.xls')
+
+    def template_format(self):
+        with open('content.html', 'w', encoding='utf-8') as f:
+            for content in self.contents:
+                f.write(str(content))
+        with open('css.html', 'w', encoding='utf-8') as f:
+            for css in self.css:
+                f.write(str(css))
+
+        with open('js.html', 'w', encoding='utf-8') as f:
+            for js in self.js:
+                f.write(str(js))
+
+        with open('content.html', 'r', encoding='utf-8') as f:
+            content = f.read()
+        with open('css.html', 'r', encoding='utf-8') as f:
+            css = f.read()
+
+        with open('js.html', 'r', encoding='utf-8') as f:
+            js = f.read()
+
+        html = self.templates.format(content=content, css=css, js=js)
+        with open('templates.html', 'w', encoding='utf-8') as f:
+            f.write(html)
+
+
+if __name__ == '__main__':
+    # crawl = Crawl("https://www.liaoxuefeng.com/wiki/1016959663602400/1019418790329088", 'liaoxuefeng_python3.pdf')
+    # crawl = Crawl("https://www.liaoxuefeng.com/wiki/1252599548343744", 'liaoxuefeng_java12.pdf')
+    #crawl = Crawl('https://www.liaoxuefeng.com/wiki/1022910821149312', 'liaoxuefeng_javascript.pdf')
+    #crawl=Crawl('https://www.liaoxuefeng.com/wiki/1177760294764384','liaoxuefeng_sql.pdf')
+    crawl=Crawl('https://www.liaoxuefeng.com/wiki/896043488029600','liaoxuefeng_git.pdf')
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-##This projiect is used to grab wiki from website of 'liaoxufeng',it is a tutorial：[https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000](https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000 "廖雪峰python教程")
+#### This project is used to grab wiki from website of 'liaoxufeng',it is a tutorial：https://www.liaoxuefeng.com/
 
-PS:I started with his Python tutorial
+#### It can be used to crawl all the wiki of him
+
+Version 0.1.0
 
 1.  This is a synchronous crawler
 
@@ -8,10 +10,53 @@ PS:I started with his Python tutorial
 
 3. Use wkhtmltopdf to convert the crawled html to pdf
 
-4. I kept the script and css styles in the html file, which makes it look like you see on the web.
+4. I kept the script and CSS styles in the html file, which makes it look like you see on the web.
 
 3. Distributed and ip proxy pools are not used
 
 5.  I kept the crawled html and pdf, they are in the zip file.
-6.  
-![](./book.JPG)
+
+#### Updated: 2019/12/17
+
+Version 0.2.0
+
+Speed up the crawler, extract the required CSS, JS and content, about 7 minutes. Code structure is more reasonable.
+
+I crawled through all the tutorials, which you can download.
+
+However, there is a little disadvantage that the example code does not have syntax highlighting.  In the previous  version, it was right. It will be improved later...
+
+<hr />
+#### 本项目是从廖雪峰网站获取wiki的一个教程：https://www.liaoxuefeng.com/
+
+#### 它可以用来抓取他所有的博客
+
+
+版本0.1.0
+
+
+一、这是一个同步爬虫程序
+
+二、使用的库如下：beautifulsoup4，requests，pdfkit，您可以'pip install[package]`
+
+三、使用wkhtmltopdf将已爬取的html转换为pdf
+
+四、我将脚本和css样式保存在html文件中，这使它看起来像是在web上看到的。
+
+五、不使用分布式和ip代理池
+
+
+六、我保留了已爬取的html和pdf，它们都在zip文件中。
+
+#### 更新日期：2019/12/17
+
+版本0.2.0
+
+
+加速爬虫，提取所需的CSS，JS和内容，大约5分钟。代码结构更合理。
+
+
+我下载了所有的教程，你可以下载。
+
+
+但是，有一点缺点，示例代码没有语法突出显示。在以前的版本中，它是正确的。以后会改进的。。。
